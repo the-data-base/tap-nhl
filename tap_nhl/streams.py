4203ea2c0df668cb118568b1c527fd8d8903770b
gavh3 <gavin.hcm@gmail.com>
2022-04-24 20:05:03 -0700

    wip

--- tap_nhl/streams.py
+++ tap_nhl/streams.py
@@ -190,10 +190,14 @@ class ScheduleStream(nhlStream):
                 consecutive pagination tokens are identical.
         """
         context = context if context else {}
-        season_start_date = datetime.strptime(context["season_start_date"], "%Y-%m-%d") # from parent stream SeasonsStream
-        season_end_date = datetime.strptime(context["season_end_date"], "%Y-%m-%d").replace(tzinfo=None) # from parent stream SeasonsStream
-        override_end_date = datetime.strptime(self.config.get("override_end_date"), "%Y-%m-%d").replace(tzinfo=None) # override end date
-        context["start_date"] = datetime.strptime(season_start_date.strftime("%Y-%m-%dT%H:%M:%SZ"), "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=pytz.UTC)
+        print(">>>>", context["season_start_date"])
+        season_start_date = cast(datetime, pendulum.from_format(context["season_start_date"], "YYYY-MM-DD")).replace(tzinfo=None) # from parent stream SeasonsStream
+        print(">>>>start", season_start_date)
+        season_end_date = cast(datetime, pendulum.from_format(context["season_end_date"], "YYYY-MM-DD")).replace(tzinfo=None) # from parent stream SeasonsStream
+        print(">>>>end", season_end_date)
+        override_end_date = cast(datetime, pendulum.from_format(self.config.get("override_end_date"), "YYYY-MM-DD")).replace(tzinfo=None) # override end date
+        context["start_date"] = season_start_date
+        print(">>>>", type(context["start_date"]))
         context["next_date"] = context["start_date"] + timedelta(days=1)
         end_date = min(season_end_date, override_end_date) # if there is an end date override, use whichever ends sooner
 
